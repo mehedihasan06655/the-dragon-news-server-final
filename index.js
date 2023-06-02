aefa88855439af1fb5e1f964035a51a5757c654d
mehedihasan06655 <mehedihasan06655@gmail.com>
2023-06-02 16:19:28 +0600

    Server site done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 app.use(cors())
 
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 
 app.get('/', (req, res) =>{
     console.log(categories)
@@ -14,6 +15,26 @@ app.get('/', (req, res) =>{
 
 app.get('/categories', (req, res) =>{
     res.send(categories)
+})
+app.get('/news', (req, res)=>{
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res)=>{
+    const id = req.params.id;
+    const selectedId = news.find(n => n._id === id);
+    res.send(selectedId);
+});
+
+app.get('/categories/:id',  (req, res)=>{
+    const id = parseInt(req.params.id);
+    if(id === 0){
+        res.send(news)
+    }
+    else{
+        const categoryList = news.filter(n => parseInt(n.category_id) === id)
+        res.send(categoryList)
+    }
 })
 
 app.listen(port, ()=>{
